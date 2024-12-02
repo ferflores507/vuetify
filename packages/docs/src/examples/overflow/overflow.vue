d2ffa40499b2aa4e662e9a31f558badf16c008ee
Fernando <ferflores507@gmail.com>
2024-12-01 21:27:52 -0500

    fix(docs/overflow): lower-cased properties values (auto and hidden)

--- packages/docs/src/examples/overflow/overflow.vue
+++ packages/docs/src/examples/overflow/overflow.vue
@@ -3,7 +3,7 @@
     <v-col cols="auto">
       <v-card class="overflow-auto" height="200" width="200">
         <v-card-text>
-          <h3>Overflow Auto</h3>
+          <h3>Overflow auto</h3>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
           facilis dicta esse molestias vero hic laudantium provident nisi eos
           quasi iusto alias sequi, aut aliquid voluptatibus commodi! Minima, eum
@@ -14,7 +14,7 @@
     <v-col cols="auto">
       <v-card class="overflow-hidden" height="200" width="200">
         <v-card-text>
-          <h3>Overflow Hidden</h3>
+          <h3>Overflow hidden</h3>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
           facilis dicta esse molestias vero hic laudantium provident nisi eos
           quasi iusto alias sequi, aut aliquid voluptatibus commodi! Minima, eum
